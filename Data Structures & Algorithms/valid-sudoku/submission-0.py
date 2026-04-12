888563098f99ffb313a1abbbc57e6340a5afc2cc
Le Hoang NGUYEN <81866799+LeHoang510@users.noreply.github.com>
2026-04-12 16:54:05 +0200

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        nb_row, nb_col = 9, 9
+
+        row = [set() for _ in range(9)]
+        col = [set() for _ in range(9)]
+        block = [[set() for _ in range(3)] for _ in range(3)]
+
+
+        for i in range(nb_row):
+            for j in range(nb_col):
+                val = board[i][j]
+                if val == ".":
+                    continue
+                if val in row[i] or val in col[j] or val in block[i//3][j//3]:
+                    return False
+                else:
+                    row[i].add(val)
+                    col[j].add(val)
+                    block[i//3][j//3].add(val)
+        
+        return True
